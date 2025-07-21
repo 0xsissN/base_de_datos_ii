ac1cc757a7a882e9344360bd9352e5ce95faf7cc
orlando-az <tj.orlando.aguilera.z@upds.net.bo>
2025-07-21 15:51:39 -0400

    Add additional SQL exercises for views, focusing on student course completion and task submissions

--- ejercicios/05_vistas.sql
+++ ejercicios/05_vistas.sql
@@ -15,7 +15,29 @@
 
 -- Ejemplo 1: Vista de usuarios con su nombre completo y correo electrónico.
 
+-- Práctica
+-- Ejercicio 1: Mostrar los estudiantes que no completaron un curso (cancelado o inscrito).
+-- Nombre completo
+-- Email
+-- Fecha formato(DD/MM/AAAA)
+-- Estado inscripcion
 
+-- Ejercicio 2: Crear una vista vista_entregas_destacadas que incluya:
+--     Nombre completo del estudiante
+--     Título de la tarea
+--     Puntaje
+--     Comentario
+--     Fecha de entrega
+
+-- Solo se incluirán entregas que tengan puntaje mayor a 85 o un comentario no nulo.
+-- Ordenar por fecha de entrega descendente.
+
+-- Ejercicio 3:Crear la vista vista_promedio_curso que muestre:
+--     Curso
+--     Promedio de calificaciones
+--     Número total de calificaciones
+
+-- Solo mostrar cursos con promedio mayor a 85 y al menos 3 calificaciones.
 
 
 
