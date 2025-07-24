74da0da4d37b57dd656ea31d18e79ea2ad39e297
Isaac Aguilera <Isaac.Aguilera@upds.edu.bo>
2025-07-24 18:13:29 -0400

    Add new SQL exercises for common table expressions (CTEs)

---
+++ ejercicios/08_ctes_common_table_expressions.sql
@@ -0,0 +1,51 @@
+
+-- Ejercicio 1: Entregas destacadas con CTE
+-- Mostrar los estudiantes que hayan realizado alguna entrega con puntaje menor a 51.
+-- Incluir su nombre, apellido y el puntaje obtenido. Ordenar por puntaje.
+
+-- Ejercicio 2: Promedio general de puntajes por curso
+-- Listar los 10 cursos con mejor desempeño general según el promedio de puntajes obtenidos en todas sus tareas entregadas.
+-- Mostrar:
+-- Nombre del curso
+-- Cantidad total de entregas
+-- Promedio de puntajes (ordenado de mayor a menor)
+
+-- Ejercicio 3: Cursos con bajo porcentaje de finalización
+-- Mostrar los cursos en los que más del 50% de las inscripciones no están en estado 'completado'. Incluir:
+
+-- Título del curso
+-- Total de inscriptos
+-- Cantidad de inscripciones no completadas
+
+-- Ejercicio 4: Usuarios sin entregas
+-- Listar a los usuarios que están inscritos en cursos con tareas asignadas, pero que no han realizado ninguna entrega. Mostrar:
+-- Nombre y apellido del usuario
+
+-- Ejercicio 5: Usuarios con porcentaje de tareas calificadas
+-- Consigna:
+-- Listar los usuarios que hayan realizado al menos 8 entregas, mostrando el porcentaje de esas entregas que han sido calificadas. 
+
+-- Mostrar:
+-- Nombre y apellido del usuario
+-- Total de entregas realizadas
+-- Total de entregas calificadas
+-- Porcentaje de entregas calificadas (ordenado de mayor a menor)
+
+-- Ejercicio 6: Cursos con mejor rendimiento que el promedio general
+-- Mostrar los cursos cuyo promedio de puntajes en todas las entregas supera el promedio general del sistema. 
+-- Incluir:
+-- Título del curso
+-- Promedio de puntajes del curso
+
+-- Ejercicio 7: Última tarea entregada por estudiante y curso
+-- Por cada usuario en cada curso en el que haya participado, mostrar la información de la última tarea entregada, incluyendo:
+
+-- Nombre del estudiante
+-- Título del curso
+-- Título de la tarea
+-- Fecha de entrega
+-- Puntaje obtenido
+
+
+
+
